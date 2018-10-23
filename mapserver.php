baa953067e707731b45f2375bc8085f2fcdfd777
piccioli <piccioli@netseven.it>
2018-10-23 17:49:04 +0200

    Inserito loop sugli square

--- mapserver.php
+++ mapserver.php
@@ -5,7 +5,7 @@
 // TODO email
 
 // Read configuration file
-$j=json_decode(file_get_contents('config.json'),TREE);
+$j=json_decode(file_get_contents('config.json'),TRUE);
 $CL_WORKING_PATH = $j['CL_WORKING_PATH'];
 $TILES_WORKING_PATH = $j['TILES_WORKING_PATH'];
 $TILES_REMOTE_PATH = $j['TILES_REMOTE_PATH'];
@@ -18,3 +18,31 @@ echo "CL_WORKING_PATH = $CL_WORKING_PATH \n";
 echo "TILES_WORKING_PATH = $TILES_WORKING_PATH \n"; 
 echo "TILES_REMOTE_PATH = $TILES_REMOTE_PATH \n"; 
 
+
+// READING QUEUE
+/**
+[
+  [LON,LAT,z,Z],
+  [LON,LAT,z,Z]
+]
+**//
+$data = json_decode(file_get_contents($argv[1]),TRUE);
+
+foreach ($data as $square) {
+
+	// LOG INIZIO SQUARE
+
+	$LON = $square[0];
+	$LAT = $square[1];
+	$zmin = $square[2];
+	$zmax = $square[3];
+
+	echo "Processing square LON=$LON LAT=$LAT zmin=$zmin zmax=$zmax \n";
+
+	// Contour Line (script singolo)
+
+	// TL COPY + RSYNC (llop da zmin a zmx step 1)
+
+	// LOG FINE SQUARE e EMAIL
+
+}
