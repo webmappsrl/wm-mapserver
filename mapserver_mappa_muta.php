386c1d2613191c51a98f89e349d46b8c892b7301
Barbieri Marco <marcobarbieri@webmapp.it>
2019-10-14 15:31:17 +0200

    Nuova procedura per "mappa muta"

---
+++ mapserver_mappa_muta.php
@@ -0,0 +1,53 @@
+<?php
+
+require('mail.php');
+
+// Read configuration file
+$j=json_decode(file_get_contents('config.json'),TRUE);
+$CL_WORKING_PATH = $j['CL_WORKING_PATH'];
+$TILES_WORKING_PATH = $j['TILES_WORKING_PATH'];
+
+echo "\n\nWEBMAPP SERVER\n\n";
+
+echo "PARAMETERS:\n";
+
+echo "CL_WORKING_PATH = $CL_WORKING_PATH \n";
+echo "TILES_WORKING_PATH = $TILES_WORKING_PATH \n";
+
+$email_footer = "CL_WORKING_PATH = $CL_WORKING_PATH<br />
+                 TILES_WORKING_PATH = $TILES_WORKING_PATH<br />;
+
+// READING QUEUE
+$data = json_decode(file_get_contents($argv[1]),TRUE);
+
+foreach ($data as $square) {
+
+	// LOG INIZIO SQUARE
+	$start=date('c');
+
+	$LON = $square[0];
+	$LAT = $square[1];
+
+	echo "Processing square LON=$LON LAT=$LAT  \n";
+
+
+	// TL COPY + RSYNC (zoom 13 e 14)
+	for ($z=13; $z <= 14; $z++) {
+		$cmd = "bash /root/wm-mapserver/generate_mappa_muta_tiles.sh $LON $LAT $z $TILES_WORKING_PATH $TILES_REMOTE_PATH";
+		echo "Executing command $cmd\n";
+		system("$cmd");
+	}
+
+	// EMAIL
+	$stop = date('c');
+	$to='marcobarbieri@webmapp.it';
+	$subj='WM-MAPSERVER';
+	$cont="LON=$LON LAT=$LAT DONE!<br />";
+	$cont.="START: $start <br/>";
+	$cont.="STOP: $stop<br/>";
+	$cont.="$email_footer";
+	sendEmail($to,$subj,$cont);
+
+	// LOG FINE SQUARE e EMAIL
+	echo "\n";
+}
