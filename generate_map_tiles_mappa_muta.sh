6b7b70bd29f18efad209dfbf8dceb7e41d98e757
Barbieri Marco <marcobarbieri@webmapp.it>
2020-01-11 14:43:33 +0100

    aggiunto processo per generare mbtiles mappa sfumo e uso suolo

---
+++ generate_map_tiles_mappa_muta.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+## creato da marco barbieri, 2018
+## licenza GPL versione 3
+## genera le tiles della mappa 1 grado per 1 grado
+
+## PARAMETERS
+## 1 LON
+## 2 LAT
+## 3 ZOOM
+## 4 WORKING_PATH
+## 5 TILES_REMOTE_PATH
+
+LON=$1
+LAT=$2
+LON2=$(expr $LON + 1)
+LAT2=$(expr $LAT + 1)
+ZOOM=$3
+WORKING_PATH=$4
+TILES_REMOTE_PATH=$5
+
+echo "CREATING TILES LON:$LON-$LON2 LAT:$LAT-$LAT2 ZOOM:$ZOOM"
+
+cd $WORKING_PATH
+rm -rf map
+echo tl copy -z $ZOOM -Z $ZOOM -b "$LON $LAT $LON2 $LAT2" http://localhost:8080/{z}/{x}/{y}.png mbtiles://./tiles.mbtiles
+tl copy -z $ZOOM -Z $ZOOM -b "$LON $LAT $LON2 $LAT2" http://localhost:8080/{z}/{x}/{y}.png mbtiles://./E${LON}N${LAT}.mbtiles &> /dev/null
+
+## START RSYNC (non includere il metadata.json)
+echo rm -f map/metadata.json
+rm -f map/metadata.json &> /dev/null
+echo rsync -avz map/ $TILES_REMOTE_PATH
+rsync -avz map/ $TILES_REMOTE_PATH &> /dev/null
+echo psql -U webmapp -d general -h localhost -c "update grid_1x1 set z$ZOOM = CURRENT_DATE WHERE grid_1x1.left = $LON AND grid_1x1.bottom = $LAT;"
+psql -U webmapp -d general -h localhost -c "update grid_1x1 set z$ZOOM = CURRENT_DATE WHERE grid_1x1.left = $LON AND grid_1x1.bottom = $LAT;" &> /dev/null
