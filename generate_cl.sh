a985d5e88a323691e75bc02e5232f7cfd8165d78
Barbieri Marco <marcobarbieri@webmapp.it>
2020-01-11 13:22:29 +0100

    Aggiunto export shapefile delle curve di livello

--- generate_cl.sh
+++ generate_cl.sh
@@ -138,3 +138,6 @@ psql -U webmapp -d general -h localhost -c "DROP TABLE cl_hr_subd;"
 psql -U webmapp -d general -h localhost -c "CREATE TABLE cl_hr_subd AS SELECT gid, st_subdivide(geom) as geom, quota, cella, type from contourlines_hr;"
 psql -U webmapp -d general -h localhost -c "CREATE INDEX cl_hr_subd_geom_idx  ON cl_hr_subd  USING GIST (geom);"
 psql -U webmapp -d general -h localhost -c "VACUUM ANALYZE cl_hr_subd;"
+
+# export curve di livello
+ogr2ogr -f "ESRI Shapefile" -overwrite /root/contourlines_hr/E${LON}N${LAT}.shp -nlt LINESTRING "PG: dbname=general user=webmapp host=localhost" -sql "SELECT * from cl_hr_subd"
