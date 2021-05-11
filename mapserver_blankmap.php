086a7d94933f5828fcd44a3494e3458235123393
Barbieri Marco <marcobarbieri@webmapp.it>
2021-05-11 10:16:37 +0200

    Rinominato file. Aggiornato codice per gestire creazione della blankmap.

--- mapserver_blankmap.php
+++ mapserver_blankmap.php
@@ -8,11 +8,10 @@
 require('mail.php');
 
 // Read configuration file
-$j=json_decode(file_get_contents('config.json'),TRUE);
+$j=json_decode(file_get_contents('blankmap_config.json'),TRUE);
 $CL_WORKING_PATH = $j['CL_WORKING_PATH'];
 $TILES_WORKING_PATH = $j['TILES_WORKING_PATH'];
 $TILES_REMOTE_PATH = $j['TILES_REMOTE_PATH'];
-$TILES_REMOTE_PATH2 = $j['TILES_REMOTE_PATH2'];
 
 echo "\n\nWEBMAPP SERVER\n\n";
 
@@ -21,7 +20,6 @@ echo "PARAMETERS:\n";
 echo "CL_WORKING_PATH = $CL_WORKING_PATH \n";
 echo "TILES_WORKING_PATH = $TILES_WORKING_PATH \n";
 echo "TILES_REMOTE_PATH = $TILES_REMOTE_PATH \n\n\n";
-echo "TILES_REMOTE_PATH2 = $TILES_REMOTE_PATH2 \n\n\n";
 
 $email_footer = "CL_WORKING_PATH = $CL_WORKING_PATH<br />
                  TILES_WORKING_PATH = $TILES_WORKING_PATH<br />
@@ -40,25 +38,25 @@ foreach ($data as $square) {
 	$zmin = $square[2];
 	$zmax = $square[3];
 
-	echo "Processing square LON=$LON LAT=$LAT z=16 \n";
+	echo "Processing square LON=$LON LAT=$LAT zmin=$zmin zmax=$zmax \n";
 
 	// Contour Line (script singolo)
 	$cmd="bash /root/wm-mapserver/generate_cl.sh $LON $LAT $CL_WORKING_PATH";
 	echo "Executing command $cmd\n";
 	system($cmd);
 
-  // TL COPY + RSYNC (zoom 16)
- 	for ($z=15; $z <= 15; $z++) {
- 		$cmd = "bash /root/wm-mapserver/generate_map_tiles_mappa_muta.sh $LON $LAT $z $TILES_WORKING_PATH $TILES_REMOTE_PATH";
- 		echo "Executing command $cmd\n";
- 		system("$cmd");
- 	}
+	// TL COPY + RSYNC (llop da zmin a zmx step 1)
+	for ($z=$zmin; $z <= $zmax; $z++) {
+		$cmd = "bash /root/wm-mapserver/generate_map_tiles.sh $LON $LAT $z $TILES_WORKING_PATH $TILES_REMOTE_PATH";
+		echo "Executing command $cmd\n";
+		system("$cmd");
+	}
 
 	// EMAIL
 	$stop = date('c');
 	$to='info@webmapp.it';
-	$subj='WM-MAPSERVER-MAPPA-MUTA';
-	$cont="LON=$LON LAT=$LAT z=16 DONE!<br />";
+	$subj='WM-MAPSERVER';
+	$cont="LON=$LON LAT=$LAT zmin=$zmin zmax=$zmax DONE!<br />";
 	$cont.="START: $start <br/>";
 	$cont.="STOP: $stop<br/>";
 	$cont.="$email_footer";
